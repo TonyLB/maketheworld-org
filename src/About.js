0e707f83bfaa5853bd7d56f2bf25970f5e3adbae
Anthony Lower-Basch <anthony.lowerbasch@gmail.com>
2020-06-10 12:14:44 -0400

    First draft of About Us picture headers

--- src/About.js
+++ src/About.js
@@ -5,6 +5,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import Avatar from '@material-ui/core/Avatar'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(() => ({
@@ -16,6 +17,9 @@ const useStyles = makeStyles(() => ({
         height: "100%"
     },
     paper: {
+        height: "100%"
+    },
+    header: {
     }
 }))
 
@@ -23,11 +27,17 @@ function About() {
     const classes = useStyles()
     return (
         <div style={{ maxWidth: "1200px" }}>
-            <Grid container>
-                <Grid item xs={6} spacing={4}>
+            <Grid container spacing={4}>
+                <Grid item xs={6}>
                     <Paper elevation={0} className={classes.paper}>
                         <Card className={classes.card}>
-                            <CardHeader title="Meguey Baker" />
+                            <CardHeader
+                                className={classes.header}
+                                title="Meguey Baker"
+                                titleTypographyProps={{ variant: "h5" }}
+                                avatar={<Avatar alt="Meg" src="/images/Meg.jpg" style={{ width: "60px", height: "60px", border: "2px solid #a342fe" }} />}
+                                style={{ backgroundColor: "#c98fff" }}
+                            />
                             <CardContent>
                                 Meg's a badass game designer, with such games as Apocalypse World, PsiRun  and Under Hollow Hills to her name, and runs LARP at the
                                 local high school for 25 students a year. She spent most of the 1990s playing and moderating MUSHs.
@@ -38,7 +48,13 @@ function About() {
                 <Grid item xs={6}>
                     <Paper elevation={0} className={classes.paper}>
                         <Card className={classes.card}>
-                            <CardHeader title="Tony Lower-Basch" />
+                            <CardHeader
+                                className={classes.header}
+                                title="Tony Lower-Basch"
+                                titleTypographyProps={{ variant: "h5" }}
+                                avatar={<Avatar alt="Tony" src="/images/FennecFox.jpg" style={{ width: "60px", height: "60px", border: "2px solid #ffc81c" }} />}
+                                style={{ backgroundColor: "#ffe9a4" }}
+                            />
                             <CardContent>
                                 Tony dissects and builds systems.  He's been building ways for gamers to come together for decades, some successful, many not.
                                 Bringing mathematics and computers together with that is a rare treat.
@@ -50,7 +66,13 @@ function About() {
                 <Grid item xs={6}>
                     <Paper elevation={0} className={classes.paper}>
                         <Card className={classes.card}>
-                            <CardHeader title="William Nichols" />
+                            <CardHeader
+                                className={classes.header}
+                                title="William Nichols"
+                                titleTypographyProps={{ variant: "h5" }}
+                                avatar={<Avatar alt="William" src="/images/William.jpg" style={{ width: "60px", height: "60px", border: "2px solid #1369e1" }} />}
+                                style={{ backgroundColor: "#93bffc" }}
+                            />
                             <CardContent>
                                 William verbs games, and manages software projects professionally. He's been building communities of gamers for years, and knows he
                                 doesn't quite get video-based online RPGs -- he's the sort whose favorite video game is a kindle.
